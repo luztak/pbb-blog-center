122fee478dcc1007bbe6c7b82653592d44210b79
Luztak Mozill <m@luztak.net>
2013-01-29 07:52:42 -0800

    Blog & feed list

---
+++ list.py
@@ -0,0 +1,10 @@
+#coding = utf-8
+__author__ = "luztak"
+
+"""The list of our blogs and blog feeds."""
+
+list = [
+    ("whtsky", "http://whouz.com", "http://whouz.com/feed.xml"),
+    ("PotatoBrother", "http://ipotato.me", "http://ipotato.me/feed.xml"),
+    ("linyis", "http://linyis.tk", "http://linyis.tk/feed.xml")
+    ]
