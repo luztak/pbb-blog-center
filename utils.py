f28f288d81ea05387a2b156adc7f43ab1c1b1e86
Luztak Mozill <m@luztak.net>
2013-01-30 21:04:15 +0800

    get_feeds completed.

--- utils.py
+++ utils.py
@@ -5,10 +5,29 @@ __author__ = "luztak"
 
 import feedparser
 
-def get_feed(feedurl):
-  """Return original data fetched by feedparser."""
-  return feedparser.parse(feedurl)
+"""Our feed format:
+feed[title,
+    (id),
+    #author #no way to get yet
+    url,
+    published,
+    updated,
+    summary,
+    content
+    ]
+"""
 
-def feedize(feed):
-  """Change feedparser data to our format.?""
-  pass
+
+def get_feeds(feedurl):
+    """Return original data fetched by feedparser."""
+    origins = feedparser.parse(feedurl).entries
+    return [{
+        title:entry.title,
+        #id:entry.id,
+        url:entry.content.base,
+        published:convert_time(entry.published_parsed),
+        updated:convert_time(entry.updated_parsed),
+        summary:entry.summary or entry.content.value,
+        content:entry.content
+        }
+        for entry in origins]
