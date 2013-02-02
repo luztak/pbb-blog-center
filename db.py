305af70c558449e4553cf1d38d190dae966ef51a
Luztak Mozill <m@luztak.net>
2013-02-01 22:35:03 -0800

    Add db.py

---
+++ db.py
@@ -0,0 +1,12 @@
+#coding = utf-8
+__author__ = "luztak"
+
+"""DB shell of Blog Center.
+"""
+
+class db(object): 
+    def __init__(self):
+        pass
+    
+    def update(self, record): 
+        pass
