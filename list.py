da66e9a3e4dd398b8d4eb426e2f6ad914ddeacea
Luztak Mozill <m@luztak.net>
2013-02-02 13:31:21 +0800

    Better var name.

--- list.py
+++ list.py
@@ -7,7 +7,7 @@ AUTHOR_NAME = 0
 BLOG_URL = 1
 FEED_URL = 2
 
-list = [
+feedlist = [
     ("whtsky", "http://whouz.com", "http://whouz.com/feed.xml"),
     ("PotatoBrother", "http://ipotato.me", "http://ipotato.me/feed.xml"),
     ("linyis", "http://linyis.tk", "http://linyis.tk/feed.xml")
